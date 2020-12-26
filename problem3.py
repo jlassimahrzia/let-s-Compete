a1080034afb638d1d016c43f22dfccd833198b03
skyllilole <76566246+skyllilole@users.noreply.github.com>
2020-12-26 20:50:53 +0100

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,1 +1,19 @@
 
+L = []
+
+ch=str(input("donner chaine : "))
+k=0
+
+for i in range(len(ch)):
+    h=i+ord(ch[i])-65
+    if h>k:
+        k=h
+k+=1
+for i in range(k):
+    L.append(' ')
+for i in range(len(ch)):
+    u=i+ord(ch[i])-65
+    L[u]=ch[i]
+
+str1 = ''.join([str(elem) for elem in L]) 
+print(str1)
